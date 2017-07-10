81d43c1e5bb526e6b847047b08a325ca5f70e275
Frank Taylor <Frank@Dino.local>
2017-07-10 16:37:25 -0700

    updated readme and beautified contents

---
+++ README.md
@@ -0,0 +1,25 @@
+# A Clone of the Classic Arcade Game, Frogger
+
+## Play Game
+
+1. Download the `.zip` file for this repository.
+2. Open `index.html`, choose a player from the home menu and the game will start automatically! Use your keyboard  keys ( ⬅️⬇️➡️⬆️ ) to move across the board.
+3. Goal of the game: reach the water blocks at the top of the screen without getting eaten by a bug.
+
+## Rules
+
+* If your player and bug collide, player will lose a point and automatically return to home position.
+* If your player reaches the water, he/she will gain a point and automatically return to home position
+* Each time your player achieves 10 points, he/she moves up one level. Once your player reaches a new level, he/she retains that level permanently. However, you cannot save your progress if you close the browser window!
+
+* * *
+
+## Build
+
+This game uses the Javascipt Canvas API and OOJS techniques to draw animation frames and trigger game events. If you want to change how the game operates, read further.
+
+**Core Functionality**
+`app.js` relies on `init.js` to determine the image that will represent the player on the canvas. Aside from that, `app.js` provides the majority of the app's functionality.
+
+**Important notes about engine.js**
+Note that `engine.js` loops through `checkCollisions()`, along with the update and render methods on the `allEnemies` array and the `Player` object *at `window.requestAnimationRate()`*. For this reason, the global `Game` object - which is used to control the number of enemies on the canvas - is separated from the `Player` and `Enemy` objects. The `Game` object controls memory leak by adding and removing enemies on the canvas every few seconds, instead of running calculations at `window.requestAnimationRate()`.
--- js/app.js
+++ js/app.js
@@ -1,49 +1,73 @@
-// Initiate event listeners for mobile and desktop via jQuery
-$(function() {
-    document.addEventListener('keyup', function(e) {
-        var allowedKeys = {
-            37: 'left',
-            38: 'up',
-            39: 'right',
-            40: 'down'
-        };
-        player.handleInput(allowedKeys[e.keyCode]);
-    });
-    $('#controls span').on("click", function() {
-        var direction = $(this).attr('data-navigate');
-        player.handleInput(direction);
-    });
-    $('#controls span').on("touchstart", function(e) {
-        e.preventDefault();
-        var direction = $(this).attr('data-navigate');
-        player.handleInput(direction);
-    });
-});
+"use strict;"
 
-
-var util = {
-    randomNumber: function(min, max) {
+var randomInteger = {
+    inRange: function(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     },
-    homeX: 210,
-    homeY: 480
+    inArray: function(array) {
+        var theValues = Array.from(array);
+        return theValues[Math.floor(theValues.length * Math.random())];
+    }
 };
 
+var score = 0,
+    level = 1,
+    pause = $('#pause'),
+    play = $('#play'),
+    playerImg = $('img.player');
 
-// Create Game object
-var Game = function(enemyCount, speed) {
+var layout = {
+    cHeight: 606,
+    cWidth: 505,
+    homeX: 202,
+    homeY: 480,
+    updateCounter: function(newScore, selector) {
+        return $(""+selector+"").hide().delay(50).fadeIn(250).attr('tabindex', newScore).text(newScore);
+    },
+    // Return left boundary between window edge and canvas
+    boundaryLeft: function() {
+        return -((window.innerWidth - layout.cWidth) / 2);
+    }
+};
+
+var moveX = layout.cHeight / 6,
+    moveY = layout.cWidth / 5;
+
+// Create Game object, define number of enemies to be created at specified interval. enemyInterval is set here in the Game object but not referenced until the generateEnemies function at the end of this file.
+var Game = function(enemyCount) {
     this.enemyCount = enemyCount;
-    this.speed = speed;
+    this.enemyInterval = randomInteger.inRange(500, 670);
     this.score = score = 0;
+    this.level = level = 1;
 };
 
 Game.prototype = Object.create(Game.prototype);
 Game.prototype.constructor = Game;
 
-
-Game.prototype.addScore = function() {
-    return $('#counter').hide().delay(50).fadeIn(250).attr('tabindex', score++).text(score);
+// Use brower size to set initial enemy x and y coordinates
+Game.prototype.enemyRangeX = function() {
+    return randomInteger.inRange(layout.boundaryLeft() * 1.5, layout.boundaryLeft() * 0.5);
 };
+
+Game.prototype.enemyRangeY = function() {
+    return randomInteger.inArray([102, 169, 270]);
+};
+
+// Increment HTML counter every time player reaches water
+Game.prototype.updateScore = function(counter) {
+    if (counter === 'add') {
+        score++;
+    }
+    if (counter === 'remove' && (score > 0)) {
+        score--;
+    }
+    if (score / level === 10) {
+        level++;
+        layout.updateCounter(level, '#level');
+    }
+    return layout.updateCounter(score, '#counter');
+};
+
 
 // Create Character constructor
 var Character = function(x, y, avatar, height, width) {
@@ -55,119 +79,158 @@ var Character = function(x, y, avatar, height, width) {
 };
 
 
-// Create Enemy object
-var Enemy = function(x, y, avatar, height, width) {
-    this.avatar = document.getElementById('enemy');
-    Character.call(this, x, y, this.avatar, this.height, this.width);
+var Enemy = function(x, y, avatar, height, width, speed) {
+    Character.call(this, x, y, document.getElementById('enemy'), this.height, this.width);
+    this.speed = randomInteger.inRange(125, 150);
 };
 Enemy.prototype = Object.create(Character.prototype);
 Enemy.prototype.constructor = Enemy;
 
-Enemy.prototype.update = function(x, y, dt) {
-    var i = 0;
-    var speed = util.randomNumber(1.75, 5.5);
-    Number(speed * dt);
-    while (i < speed) {
-        this.x++;
-        i++;
-    }
+// Set enemy movement across x-axis to a random number of pixels between 4 and 7. Multiply movement by dt (Date time) function to ensure cross-browser and cross-OS consistency
+Enemy.prototype.update = function(dt) {
+    this.x = this.x + (this.speed * dt);
 };
 
 Enemy.prototype.render = function(x, y, avatar) {
     ctx.drawImage(this.avatar, this.x, this.y);
 };
 
-
 var Player = function(x, y) {
-    this.avatar = document.getElementById('hero');
-    Character.call(this, x, y, this.avatar, this.height, this.width);
+    Character.call(this, x, y, document.getElementById('hero'), this.height, this.width);
 };
 
 Player.prototype = Object.create(Character.prototype);
 Player.prototype.constructor = Player;
 
-
-Player.prototype.update = function() {
-    return;
-};
-
-
+// Return player to initial position set in layout data
 Player.prototype.squareOne = function() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    this.x = util.homeX;
-    this.y = util.homeY;
+    ctx.clearRect(0, 0, layout.cWidth, layout.cHeight);
+    this.x = layout.homeX;
+    this.y = layout.homeY;
     return;
 };
-
-// var score = 0;
 
 Player.prototype.render = function() {
     ctx.drawImage(this.avatar, this.x, this.y);
 };
 
 Player.prototype.handleInput = function(input) {
-    var distanceY = canvas.height / 6.3;
-    var distanceX = canvas.width / 5;
-
     // Respond to keyup event by moving left, right, up, or down.
     switch (input) {
         case 'up':
-            this.y -= distanceY;
+            this.y -= moveY
             break;
         case 'down':
-            this.y += distanceY;
+            this.y += moveY
             break;
         case 'left':
-            this.x -= distanceX;
+            this.x -= moveX
             break;
         case 'right':
-            this.x += distanceX;
+            this.x += moveX
             break;
         default:
             return;
     }
 
     // If player moves off map, return to home position
-    if (this.x < -10 || this.x > 415 || this.y > (util.homeY + 20)) {
+    if (this.x < -10 || this.x > 415 || this.y > (layout.homeY + 20)) {
         return this.squareOne();
     }
-    if (this.y < -45) {
-        return this.squareOne(), game.addScore();
+    // If player reaches water, return to home position and increment game score
+    if (this.y < 90) {
+        return this.squareOne(), game.updateScore('add');
     }
 };
 
-
-var game = new Game(3, util.randomNumber(400, 570)),
-    player = new Player(util.homeX, util.homeY),
-    allEnemies = [];
-
-var checkCollisions = function() {
-    allEnemies.forEach(function(elem, i, arr) {
-        setTimeout(function() {
-            if (elem.x > 0 && elem.x < 500) {
-                if (elem.x < player.x + player.width &&
-                    elem.x + elem.width > player.x &&
-                    elem.y < (player.y) + (player.height) &&
-                    elem.height + elem.y > (player.y)) {
-                    return player.squareOne();
-                }
-            }
-        }, 100);
-    });
+// Initialize Game, Player and allEnemies. Game and Player Parameters are defined in toggleCanvas
+Game.prototype.toggleKeys = function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+    player.handleInput(allowedKeys[e.keyCode]);
 };
 
-function generateEnemies() {
-    var enemy = new Enemy(util.randomNumber(-220, -300), util.randomNumber(120, 220));
+// Show canvas, selected avatar and enable keyboard direction keys
+Game.prototype.turnOn = function() {
+    $('.player-selection').hide();
+    $('#canvas').addClass('active');
+    document.addEventListener("keyup", this.toggleKeys);
+    pause.show();
+    play.hide();
+    this.enemyCount = 3;
+};
 
-    var cHeight = $('#canvas').height(),
-        cWidth = $('#canvas').width();
+Game.prototype.pause = function() {
+    document.removeEventListener("keydown", this.toggleKeys);
+    pause.hide();
+    play.show();
+    this.enemyCount = 0;
+};
+
+// Hide canvas, allow re-selection of avatar and disable keyboard direction keys
+Game.prototype.levelUp = function() {
+    return layout.updateCounter(level++, '#level');
+};
+
+var game = new Game(),
+    player = new Player(),
+    allEnemies = [];
+
+// Generate number of enemies specified in game params, and place each enemy
+// at x and y coordinates specified in Game.prototype object
+function generateEnemies() {
+    var enemy = new Enemy(game.enemyRangeX(), game.enemyRangeY());
 
     for (var i = 0; i < game.enemyCount; i++) {
         allEnemies.push(enemy);
-        console.log(allEnemies.length);
-        if (allEnemies.length > game.enemyCount * 3) {
+
+        // Regularly dump enemies from allEnemies array to prevent memory leak
+        if (allEnemies.length > game.enemyCount * 6) {
             return allEnemies.splice(0, game.enemyCount);
         }
     }
 }
-setInterval(generateEnemies, game.speed);
+setInterval(generateEnemies, game.enemyInterval);
+
+
+function choosePlayer(e) {
+    $(this).attr('id', 'hero').addClass('moveDown').siblings().removeAttr('id').hide();
+    setTimeout(game.turnOn(), 450);
+    player = new Player(layout.homeX, layout.homeY);
+}
+playerImg.on('click', choosePlayer);
+
+
+function pauseGame() {
+    return game.pause();
+}
+pause.on('click', pauseGame);
+
+function resumeGame() {
+    return game.turnOn();
+}
+play.on('click', resumeGame);
+
+// Check collisions using Axis-Aligned Bounding Box method for collision detection
+var checkCollisions = function() {
+    allEnemies.forEach(function(elem, i, arr) {
+
+        var characterArr = [player.width, player.height, elem.width, elem.height];
+        var charBubbles = characterArr.map(function(num) {
+            return num * 0.75;
+        });
+
+        if (elem.x > 0 && elem.x < 500) {
+            if (elem.x < player.x + charBubbles[0] &&
+                elem.x + charBubbles[2] > player.x &&
+                elem.y < (player.y) + (charBubbles[1]) &&
+                charBubbles[3] + elem.y > (player.y)) {
+                    return player.squareOne(), game.updateScore("remove");
+            }
+        }
+    });
+};
