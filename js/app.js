2c0ce4c3f872553121ca888c32c6e9e74d31a45d
Frank Taylor <frank@franktaylor.io>
2017-09-07 15:55:00 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,9 @@
-// This is the code for a JavaScript clone of the classic Arcade Game, Frogger
-// To see full project, go to https://github.com/shagamemnon/frogger/archive/master.zip
+var layout = '<div class="game-title" style="position:fixed;left:0;right:0;top:100px;text-align:center"><h1>Frogger</h1><h3>Click a character to get started.</h3></div><div class="player-selection"> <img class="player" src="https://shagamemnon.github.io/frogger/images/char-boy.png" id="hero" alt="char-boy"><img class="player" src="https://shagamemnon.github.io/frogger/images/char-princess-girl.png" alt="char-princess-girl"> <img class="player" src="https://shagamemnon.github.io/frogger/images/char-pink-girl.png" alt="char-pink-girl"> </div><img id="enemy" src="https://shagamemnon.github.io/frogger/images/enemy-bug.png" style="display:none" alt="enemy"> <canvas id="canvas" class="game-canvas" style="margin-top:5px"></canvas> <div id="scoreboard" class="game-scoreboard"> <div class="row"><span>Level</span> <br><span id="level" tabindex="1">1</span> </div><div class="row"><span>Score</span> <br><span id="counter" class="game-score" tabindex="0">0</span> </div></div><div class="pause pause-button"></div><div class="play play-button"></div>';
 
-// Create DOM tree
-var layout = '<div class="game-title" style="position:fixed;left:0;right:0;top:100px;text-align:center"><h1>Frogger</h1><h3>Click a character to get started.</h3></div><div class="player-selection"> <img class="player" src="images/char-boy.png" id="hero" alt="char-boy"><img class="player" src="images/char-princess-girl.png" alt="char-princess-girl"> <img class="player" src="images/char-pink-girl.png" alt="char-pink-girl"> </div><img id="enemy" src="images/enemy-bug.png" style="display:none" alt="enemy"> <canvas id="canvas" class="game-canvas" style="margin-top:5px"></canvas> <div id="scoreboard" class="game-scoreboard"> <div class="row"><span>Level</span> <br><span id="level" tabindex="1">1</span> </div><div class="row"><span>Score</span> <br><span id="counter" class="game-score" tabindex="0">0</span> </div></div><div class="pause pause-button"></div><div class="play play-button"></div>';
-
-// NOTE: innerHTML is an insecure technique for populating the DOM and should not be used in production environments
-// it is used here for convenience
+// NOTE: innerHTML is an insecure technique for populating the DOM and should not be used in production environments. it is used here for convenience
 document.body.innerHTML = layout;
-
 document.addEventListener("DOMContentLoaded", function(event) {
-    var css = "\\\\#canvas,.player,body{text-align:center}body{margin:auto;font-family:'Helvetica Neue',sans-serif}.player-selection{display:flex;justify-content:center;transition:.3s}.player{display:inline-flex;justify-content:center;cursor:pointer;height:100px;width:auto;margin-top:400px;transition:.4s}#canvas,.game-scoreboard{display:block;margin:auto}.moveDown{display:block;margin-top:490px;transition:.4s}#canvas{overflow:hidden;position:fixed;top:0;left:0;right:0;transition:0s;opacity:0;visibility:hidden}.active{visibility:visible!important;opacity:1!important;transition:350ms ease-out}.game-scoreboard{height:32px;padding-top:30px;width:600px}.game-scoreboard .row{display:flex;width:49%;margin:auto;justify-content:center;text-align:center;flex-direction:column}.game-scoreboard .row:first-child{float:left;color:#aaa}.game-scoreboard .row:last-child{float:right}.game-scoreboard .row span:first-child{font-size:20px;line-height:10px;text-transform:uppercase}.game-scoreboard .row span:last-child{font-size:48px;line-height:20px}.game-score{padding:5px;font-weight:700}.pause-button,.play-button{background-position:bottom;height:100px;background-repeat:no-repeat;margin-top:600px;position:fixed;left:0;right:0;cursor:pointer;display:none}.pause-button{background-image:url(./images/pause.png);background-size:auto 100px}.play-button{background-image:url(./images/play.png);background-size:auto 80px}\\\\",
+    var css = "\\\\#canvas,.player,body{text-align:center}body{margin:auto;font-family:'Helvetica Neue',sans-serif}.player-selection{display:flex;justify-content:center;transition:.3s}.player{display:inline-flex;justify-content:center;cursor:pointer;height:100px;width:auto;margin-top:400px;transition:.4s}#canvas,.game-scoreboard{display:block;margin:auto}.moveDown{display:block;margin-top:490px;transition:.4s}#canvas{overflow:hidden;position:fixed;top:0;left:0;right:0;transition:0s;opacity:0;visibility:hidden}.active{visibility:visible!important;opacity:1!important;transition:350ms ease-out}.game-scoreboard{height:32px;padding-top:30px;width:600px}.game-scoreboard .row{display:flex;width:49%;margin:auto;justify-content:center;text-align:center;flex-direction:column}.game-scoreboard .row:first-child{float:left;color:#aaa}.game-scoreboard .row:last-child{float:right}.game-scoreboard .row span:first-child{font-size:20px;line-height:10px;text-transform:uppercase}.game-scoreboard .row span:last-child{font-size:48px;line-height:20px}.game-score{padding:5px;font-weight:700}.pause-button,.play-button{background-position:bottom;height:100px;background-repeat:no-repeat;margin-top:600px;position:fixed;left:0;right:0;cursor:pointer;display:none}.pause-button{background-image:url(./https://shagamemnon.github.io/frogger/images/pause.png);background-size:auto 100px}.play-button{background-image:url(./https://shagamemnon.github.io/frogger/images/play.png);background-size:auto 80px}.html-space {position:fixed;height:500px;width:200px;left:0;top:0;background:black}\\\\",
         head = document.head || document.getElementsByTagName('head')[0],
         style = document.createElement('style');
 
@@ -205,16 +199,7 @@ Player.prototype.handleInput = function(input) {
 };
 
 
-// Initialize Game, Player and allEnemies.
-Game.prototype.toggleKeys = function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-    player.handleInput(allowedKeys[e.keyCode]);
-};
+
 
 
 // Show canvas, selected avatar and enable keyboard direction keys
@@ -226,9 +211,8 @@ Game.prototype.start = function() {
     this.enemyCount = 3;
 };
 
-
 Game.prototype.pause = function() {
-    document.removeEventListener("keydown", this.toggleKeys);
+    document.removeEventListener("keyup", this.toggleKeys);
     layout.waterBreak.start();
     this.enemyCount = 0;
 };
@@ -321,4 +305,15 @@ var checkCollisions = function() {
             }
         }
     });
+};
+
+// Control key input
+Game.prototype.toggleKeys = function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+    player.handleInput(allowedKeys[e.keyCode]);
 };
